e2d3dd4d2628c6f733ad00f29c39117d4aeb2cf3
Aniruddha Thorat <aniruddhathorat123@gmail.com>
2019-10-28 21:22:15 +0530

    Commit-2 : Added some documentation for notification snippet.

--- app/src/main/java/com/aniruddha/aadpractice/MainActivity.kt
+++ app/src/main/java/com/aniruddha/aadpractice/MainActivity.kt
@@ -3,9 +3,9 @@ package com.aniruddha.aadpractice
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.RemoteInput
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -29,7 +29,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onResume() {
         super.onResume()
+        // Calls custom toast that was created
         customToast("Action !!!")
+
+        // Create Notification channel
         createNotificationChannel()
     }
 
@@ -39,6 +42,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    /**
+     * function to create the notification with given channel and had different parameters.
+     * you can remove setStyle to see download like notification.
+     */
     private fun startNotification() {
         val intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -82,6 +89,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    /**
+     * create custom toast with given string description to show
+     */
     fun customToast(data: String) {
         val inflater = layoutInflater
         val layout : ViewGroup = inflater.inflate(R.layout.custom_toast,
@@ -96,6 +106,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    /**
+     * create the channel for the notification.
+     * channel actually created only once, and used again if called again.
+     */
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = "Sample channel"
@@ -103,6 +117,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val importance = NotificationManager.IMPORTANCE_DEFAULT
             val channel = NotificationChannel(AADConstants.CHANNEL_ID, name, importance).apply {
                 description = detailText
+                enableVibration(true)
+                enableLights(true)
+                lightColor = Color.RED
             }
 
             val notificationManager : NotificationManager =
